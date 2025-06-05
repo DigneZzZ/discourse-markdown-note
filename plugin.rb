887ec502fa4b15e7bc04c2a1ed23456d6f1f84d4
Alexey <dignezzz@gmail.com>
2025-06-05 19:29:35 +0300

    remove buttons

--- plugin.rb
+++ plugin.rb
@@ -1,28 +1,15 @@
 # name: discourse-markdown-note
-# about: A BBCode Markdown extension for notes with full tag support, editor buttons, dark theme support and admin settings panel
-# version: 0.7.5
+# about: A BBCode Markdown extension for notes with full tag support, dark theme support and customizable styling
+# version: 0.7.6
 # authors: DigneZzZ
 # url: https://github.com/DigneZzZ/discourse-markdown-note
 
 register_asset "stylesheets/notifications.scss"
-register_asset "stylesheets/admin.scss"
 
 # Define settings for the plugin
 enabled_site_setting :discourse_markdown_note_enabled
 
-add_admin_route 'discourse_markdown_note', 'markdown-note'
-
 after_initialize do
-  # Load admin controller
-  require_relative "app/controllers/admin/markdown_note_controller"
-  
-  # Register admin routes for note customization
-  Discourse::Application.routes.append do
-    get '/admin/plugins/markdown-note' => 'admin/plugins#index', constraints: AdminConstraint.new
-    get '/admin/plugins/markdown-note/settings' => 'admin/markdown_note#show', constraints: AdminConstraint.new
-    post '/admin/plugins/markdown-note/settings' => 'admin/markdown_note#update', constraints: AdminConstraint.new
-  end
-
   # Register plugin note texts for translation
   register_html_builder('server:note-type-titles') do
     {
